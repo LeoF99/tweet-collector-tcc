af6b4c0641025f37152914401ea3d2b7db88fbe9
LeoF99 <leo.ferreira@dcx.ufpb.br>
2021-07-26 22:30:15 -0300

    feat(twitter): add twitter api gateway

---
+++ src/gateways/twitter.gateway.ts
@@ -0,0 +1,63 @@
+import Twit from 'twit';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+  TWIT_API_KEY,
+  TWIT_API_SECRET_KEY,
+} = process.env;
+
+interface IQuery {
+  query: string;
+  count: number;
+}
+
+interface ITweetData {
+  text: string;
+  query: string;
+  timestamp: Date;
+  createdAt: Date;
+}
+
+class TwitterGateway {
+  private readonly T: Twit;
+
+  constructor() {
+    this.T = new Twit(
+      {
+        consumer_key: String(TWIT_API_KEY),
+        consumer_secret: String(TWIT_API_SECRET_KEY),
+        app_only_auth: true,
+      },
+    );
+  }
+
+  async getTweets(data: IQuery) {
+    const tweets = await this.T.get(
+      'search/tweets',
+      {
+        q: data.query,
+        count: data.count,
+        lang: 'pt',
+        result_type: 'recent',
+      },
+    );
+
+    return this.parseTwitResult(tweets, data.query);
+  }
+
+  private parseTwitResult(twitResult: Twit.PromiseResponse, query: string) {
+    // @ts-ignore
+    const parsedData: ITweetData[] = twitResult.data.statuses.map((status) => ({
+      text: status.text,
+      query,
+      timestamp: new Date(),
+      createdAt: new Date(status.created_at),
+    }));
+
+    return parsedData;
+  }
+}
+
+export default new TwitterGateway();
